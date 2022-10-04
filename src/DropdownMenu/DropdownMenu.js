d3765eed0f18efa772e45ca8fa69c4265807cd3f
Vlad Ilyashenko <vlad180317@gmail.com>
2022-10-04 16:36:59 +0600

    Реализовал добавление объектов через бургер-меню.

--- src/DropdownMenu/DropdownMenu.js
+++ src/DropdownMenu/DropdownMenu.js
@@ -1,20 +1,24 @@
-import React, {useState} from "react";
+import React, {createRef, useState, } from "react";
 import "./DropdownMenu.css"
-
+import {addPoint} from '../Map';
+import {addPolyline} from '../Map';
+import {addPolygon} from '../Map';
 
 function DropdownMenu () {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);  
+
     return(
         <div className="DropBtnBlock">
 
             <button className="DropBtn" onClick={() => setIsOpen(prev => !prev)}>Добавить примитив</button> 
             <div className={'dropdown ' + (isOpen ? 'open' : 'closed')}>
                 <ul>
-                <li><button className="DropBtn">Точка</button></li>
-                <li><button className="DropBtn">Полилиния</button></li>
-                <li><button className="DropBtn">Полигон</button></li>
+                    <li><button className="DropBtn" onClick = {addPoint}>Точка</button></li>
+                    <li><button className="DropBtn" onClick = {addPolyline}>Полилиния</button></li>
+                    <li><button className="DropBtn" onClick = {addPolygon}>Полигон</button></li>
                 </ul>
             </div>
+
         </div>
     );
 }
--- src/Map.js
+++ src/Map.js
@@ -1,103 +1,84 @@
 import React from 'react';
 import { YMaps, Map, GeolocationControl, ZoomControl } from 'react-yandex-maps';
 import './Map.css';
-import './Menu/BurgerMenu.js'
 
 const mapState = {
     center: [54.98517806972585,73.3714099999999],
     zoom: 16
 };
 
+// Хуки
+const ymaps = React.createRef(null);
+const placemarkRef = React.createRef(null);
+const mapRef = React.createRef(null);
+const polylineRef = React.createRef(null);
+const polygonRef = React.createRef(null);
+
+// Добавление точек на карту
+export const addPoint = () => {
+    const newPlacemark = new ymaps.current.Placemark([], {},
+        {
+            editorDrawingCursor: "crosshair",
+            iconLayout: 'default#image',
+            iconImageHref: 'https://cdn-user-icons.flaticon.com/80802/80802443/1664782767840.svg?token=exp=1664783690~hmac=98af978fca0518c27f5b564fd482976c',
+            iconImageSize: [46, 46],
+            iconImageOffset: [-23, -46],
+            draggable: true
+        }    
+    );
+    placemarkRef.current = newPlacemark;
+    mapRef.current.geoObjects.add(placemarkRef.current);
+    placemarkRef.current.editor.startDrawing();
+};
+
+// Добавление полилиний на карту
+export const addPolyline = () => {
+    const newPolyline = new ymaps.current.Polyline([], {},
+        {
+            editorDrawingCursor: "crosshair",
+            editorMaxPoints: 2,
+            fillColor: "#0bbcc9",
+            strokeColor: "#0bbcc9",
+            strokeWidth: 5,
+            draggable: true
+        }
+    );
+    polylineRef.current = newPolyline;
+    mapRef.current.geoObjects.add(polylineRef.current);
+    polylineRef.current.editor.startDrawing();
+};
+
+// Добавление полигона на карту
+export const addPolygon = () => {
+    const newPolygon = new ymaps.current.Polygon([], {},
+        {
+            editorDrawingCursor: "crosshair",
+            editorMaxPoints: 5,
+            fillColor: "#b8a7a2aa",
+            strokeColor: "#0bbcc9",
+            strokeWidth: 5,
+            draggable: true
+        }
+    );
+    polygonRef.current = newPolygon;
+    mapRef.current.geoObjects.add(polygonRef.current);
+    polygonRef.current.editor.startDrawing();
+};
+
+// Рендеринг карты
 export default function Map1() {
     
-    const ymaps = React.useRef(null);
-    const placemarkRef = React.useRef(null);
-    const mapRef = React.useRef(null);
-    const polylineRef = React.useRef(null);
-    const polygonRef = React.useRef(null);
-
-    // Создание метки
-    const createPlacemark = (coords) => {
-        return new ymaps.current.Placemark(coords, 
-            {
-                balloonContentHeader: 'Point Data',
-                balloonContentBody: '[coordinates]', 
-            },
-            {
-                iconLayout: 'default#image',
-                iconImageHref: 'https://cdn-user-icons.flaticon.com/80802/80802443/1664782767840.svg?token=exp=1664783690~hmac=98af978fca0518c27f5b564fd482976c',
-                iconImageSize: [46, 46],
-                iconImageOffset: [-23, -46],
-                draggable: true 
-            }
-        );
-    };
-
-    // Обратное геокодирование
-    const getAddress = (coords) => {
-        ymaps.current.geocode(coords).then((res) => {
-            const firstGeoObject = res.geoObjects.get(0);
-        })
-    }
-    
-    // Добавление метки на карту по адресу клика
-    const addPoint = (e) => {
-        const coords = e.get("coords");
-        placemarkRef.current = createPlacemark(coords);
-        mapRef.current.geoObjects.add(placemarkRef.current);
-        placemarkRef.current.events.add("dragend", function () {
-            getAddress(placemarkRef.current.geometry.getCoordinates());
-        });
-        getAddress(coords);
-    };
-
-    // Добавление полилиний на карту
-    const addPolyline = e => {
-        const newPolyline = new ymaps.current.Polyline([], {},
-            {
-                editorDrawingCursor: "crosshair",
-                editorMaxPoints: 2,
-                fillColor: "#0bbcc9",
-                strokeColor: "#0bbcc9",
-                strokeWidth: 5
-            }
-        );
-        polylineRef.current = newPolyline;
-        mapRef.current.geoObjects.add(polylineRef.current);
-        polylineRef.current.editor.startDrawing();
-    };
-
-    // Добавление полигона на карту
-    const addPolygon = e => {
-        const newPolygon = new ymaps.current.Polygon([], {},
-            {
-                editorDrawingCursor: "crosshair",
-                editorMaxPoints: 5,
-                fillColor: "#b30bc900",
-                strokeColor: "#0bbcc9",
-                strokeWidth: 5
-            }
-        );
-        polygonRef.current = newPolygon;
-        mapRef.current.geoObjects.add(polygonRef.current);
-        polygonRef.current.editor.startDrawing();
-    };
-
-    // Рендеринг карты
     return (
         <div className="map-size map-container">
             <YMaps>
                 <Map
                     className = "map-size map-mode"
-                    modules={["Placemark", "Polyline", "Polygon", "geocode", "geoObject.addon.balloon",
-                        "geoObject.addon.editor", "layout.ImageWithContent"]}
-                    instanceRef={mapRef}
-                    onLoad={(ymapsInstance) => (ymaps.current = ymapsInstance)}
-                    //onClick={addPoint}
-                    //onClick={addPolyline}
-                    onClick={addPolygon}
-                    state={mapState} 
-                    options={{suppressMapOpenBlock: true}}
+                    modules = {["Placemark", "Polyline", "Polygon", "geocode", "geoObject.addon.balloon",
+                        "geoObject.addon.editor"]}
+                    instanceRef = {mapRef}
+                    onLoad = {(ymapsInstance) => (ymaps.current = ymapsInstance)}
+                    state = {mapState} 
+                    options = {{suppressMapOpenBlock: true}}
                 >
                     <ZoomControl 
                         options={{
@@ -108,7 +89,6 @@ export default function Map1() {
                                 top: 250,
                             }
                         }}
-                        className="zoombar-style"
                     />
                     
                     <GeolocationControl 
