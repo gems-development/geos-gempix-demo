6ccf6b512b035d6f5996e1ec472a1804f5e7d1b5
Denis <kds04042002omgu@gmail.comgit  config --global user.name DenisKamaliev>
2022-10-25 15:47:06 +0600

    Refactoring

---
+++ src/components/ui/dropdownMenu/DropdownMenu.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import "./DropdownMenu.css"
+import {removeAllObjects} from '../../workArea/Map.js';
+import {newPolyline} from '../../workArea/Map.js';
+import {newPolygon} from '../../workArea/Map.js';
+
+
+function DropdownMenu () {
+    const [isOpen, setIsOpen] = useState(false);  
+
+    return(
+        <div className="DropBtnBlock">
+
+            <button className="MainBtn" onClick={() => setIsOpen(prev => !prev)}>Добавить примитив</button> 
+            <span class="material-icons">add</span>
+
+           
+            <div className={'dropdown ' + (isOpen ? 'open' : 'closed')}>
+                <ul>
+                    <li><button className="DropBtn" onClick = {newPolyline}>Полилиния</button>
+                    <span class="material-icons">timeline</span></li>
+                    
+                     
+                    <li><button className="DropBtn" onClick = {newPolygon}>Полигон</button>
+                    <span class="material-icons">polymer</span></li>
+                    <li><button className="DropBtn" onClick = {removeAllObjects}>Очистка</button>
+                    <span class="material-symbols-outlined">delete</span></li>
+                </ul>
+            </div>
+
+        </div>
+    );
+}
+
+export default DropdownMenu
