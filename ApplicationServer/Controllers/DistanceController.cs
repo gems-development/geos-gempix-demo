17c21a2a64006f95bb23b2b742edf30eba59d5c0
Vlad Ilyashenko <vlad180317@gmail.com>
2022-10-28 18:51:17 +0600

    работа с серверной частью

---
+++ ApplicationServer/Controllers/DistanceController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApplicationServer.Model;
+
+namespace ApplicationServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DistanceController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public DistanceController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "GetDistance")]
+        public double Get([FromBody] IEnumerable<Point> points)
+        {
+            var point1 = points.FirstOrDefault();
+            var point2 = points.LastOrDefault();
+
+            return Math.Sqrt(Math.Pow(point2?.X - point1?.X ?? 0, 2) + Math.Pow(point2?.Y - point1?.Y ?? 0, 2));
+        }
+    }
+}
