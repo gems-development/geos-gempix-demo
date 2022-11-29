ad835ead62e9b66755b318e6c1d7a9ad994d9904
Vlad Ilyashenko <vlad180317@gmail.com>
2022-11-29 18:02:59 +0000

    +поддержка простр. отнош. на клиенте, shortestLine

--- ApplicationServices/Converters/GeodeticToWebMercatorCoordinateConverter.cs
+++ ApplicationServices/Converters/GeodeticToWebMercatorCoordinateConverter.cs
@@ -1,6 +1,7 @@
 ﻿using ApplicationServices.Interfaces;
 using CoordinateSharp;
 using GeometryModels;
+using GeometryModels.Models;
 
 namespace ApplicationServices.Converters;
 
@@ -16,5 +17,18 @@ internal class GeodeticTo3857CoordinateConverter : ICoordinateConverter
     {
         var convertedPoint = new Coordinate(y, x).UTM;
         return new Point(convertedPoint.Easting, convertedPoint.Northing);
+    }
+
+    public double[][] Convert(Line line)
+    {
+        var wm1 = new WebMercator(line.Point1.X, line.Point1.Y);
+        var wm2 = new WebMercator(line.Point2.X, line.Point2.Y);
+        var convertedPoint1 = WebMercator.ConvertWebMercatortoLatLong(wm1);
+        var convertedPoint2 = WebMercator.ConvertWebMercatortoLatLong(wm2);
+        return new[]
+        {
+            new[] {convertedPoint1.Latitude.ToDouble(), convertedPoint1.Longitude.ToDouble()},
+            new[] {convertedPoint2.Latitude.ToDouble(), convertedPoint2.Longitude.ToDouble()}
+        };
     }
 }
